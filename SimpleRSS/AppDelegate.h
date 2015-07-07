d9faa6717066bdd0272bd6a7ebe6dab67af6b768
Dim <dim@MacBook-Pro-Dim.local>
2015-07-07 13:40:33 +0400

    Initial Commit

---
+++ SimpleRSS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SimpleRSS
+//
+//  Created by Dim on 07.07.15.
+//  Copyright (c) 2015 Dmitriy Baklanov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SimpleRSS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SimpleRSS
+//
+//  Created by Dim on 07.07.15.
+//  Copyright (c) 2015 Dmitriy Baklanov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
